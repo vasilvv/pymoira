b84af881f800bfad13b5e90379c5f4ec0445239a
Victor Vasiliev <vasilvv@mit.edu>
2013-01-26 16:37:06 -0500

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'pymoira',
+      version = '1.0',
+      description = 'Client library for MIT Moira service managment system protocol',
+      author = 'Victor Vasiliev',
+      author_email = 'vasilvv@mit.edu',
+      url = 'https://github.com/vasilvv/pymoira',
+      license = 'MIT',
+      py_modules = ['pymoira'])
