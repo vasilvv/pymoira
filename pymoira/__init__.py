1bd0d89295b23fa8ab3d2ebcc004829b48f058e4
Victor Vasiliev <vasilvv@mit.edu>
2013-02-22 17:46:58 -0500

    Introduce a very basic Host class for ownership lookup.

--- pymoira/__init__.py
+++ pymoira/__init__.py
@@ -2,5 +2,6 @@ from client import Client
 from lists import *
 from filesys import Filesys
 from user import User
+from host import Host
 from errors import *
 import constants 
---
+++ pymoira/host.py
@@ -0,0 +1,24 @@
+#
+## PyMoira client library
+##
+## This file contains the classes which aid in dealing with Moira hosts.
+#
+
+from lists import ListMember
+import socket
+
+class Host(ListMember):
+    def __init__(self, client, name, canonicalize = True):
+        name = name.upper()
+        if '.MIT.EDU' not in name:
+            name += '.MIT.EDU'
+
+        super(Host, self).__init__(client, ListMember.Machine, name)
+        if canonicalize:
+            self.canonicalize()
+    
+    def canonicalize(self):
+        """Replaces the alias form of the host with the proper name."""
+
+        self.name = socket.getfqdn(self.name)
+
